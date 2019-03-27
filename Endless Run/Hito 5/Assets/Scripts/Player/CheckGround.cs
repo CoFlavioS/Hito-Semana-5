2e7a129d1a891039a75cd05cbe88907f74dd05d4
luislombardis <anguilaelectrica3@gmail.com>
2019-03-28 00:24:13 +0100

    Movimiento y muerte del personaje
    Eliminados scripts anteriores del movimiento del personaje

---
+++ Endless Run/Hito 5/Assets/Scripts/Player/CheckGround.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckGround : MonoBehaviour
+{
+
+    private Controles player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponentInParent<Controles>();
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = false;
+        }
+    }
+}
