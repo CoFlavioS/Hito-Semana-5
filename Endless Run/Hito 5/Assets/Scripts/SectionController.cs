692301edbfac2895141fae7d7d5c61f3b830e10a
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-03-27 20:45:15 +0100

    added level sections controller. Instanciates objects periodically

---
+++ Endless Run/Hito 5/Assets/Scripts/SectionController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSectionController : MonoBehaviour
+{
+
+    public Camera currCamera;
+    public GameObject section;
+    private Vector2 spawnPosition;
+
+    public static float levelSpeed = 2.5f;
+    public float sectionSeparation = 4f;
+
+    private float waitTime;
+    private float timer;
+
+    void Start()
+    {
+        timer = 0f;
+        waitTime = sectionSeparation / levelSpeed;
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > waitTime)
+        {
+            timer -= waitTime;
+            CreateSection();
+        }
+    }
+
+    void CreateSection()
+    {
+        if (currCamera.enabled && currCamera.orthographic)
+        {
+            spawnPosition.y = currCamera.orthographicSize;
+            spawnPosition.x = position.y * currCamera.aspect;
+            Object.Instantiate(section, spawnPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ Endless Run/Hito 5/Assets/Scripts/SectionMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionMove : MonoBehaviour
+{
+    private float scrollSpeed;
+
+    void Start()
+    {
+        scrollSpeed = LevelSectionController.levelSpeed;
+    }
+
+
+    void Update()
+    {
+
+    }
+}
