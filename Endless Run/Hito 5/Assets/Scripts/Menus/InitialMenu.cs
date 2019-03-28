06407cb5f04beb113cf0f292f7e4788148cbbf7d
Flaviu Bogdan Condor <jaguaru40@yahoo.es>
2019-03-28 17:52:40 +0100

    initial menu

---
+++ Endless Run/Hito 5/Assets/Scripts/Menus/InitialMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitialMenu : MonoBehaviour
+{
+    public GameObject initialMenuUI;
+    public bool isPlay = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlay = false;
+        initialMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Play()
+    {
+        initialMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
