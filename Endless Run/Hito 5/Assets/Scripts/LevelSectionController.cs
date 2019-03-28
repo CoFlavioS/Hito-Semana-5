41162c09f51ab6ed0dcb26a25d0fb176133c659c
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-03-28 11:55:46 +0100

    Controller selects sections to create randmoly.

--- Endless Run/Hito 5/Assets/Scripts/LevelSectionController.cs
+++ Endless Run/Hito 5/Assets/Scripts/LevelSectionController.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LevelSectionController : MonoBehaviour
 {
 
     public Camera currCamera;
-    public GameObject section;
     [HideInInspector]
     public float borderDistance;   //Distance between controller and controller
     public float bufferDistance;   //Buffer distamce so sections dont pop-in or out
     private Vector2 spawnPosition;
 
+    private GameObject section;
+    private GameObject[] commonSections;
+    private GameObject[] dropletSections;
+    private GameObject[] sunSections;
+    private int sectionCounter = 0;
+
     public float levelSpeed = .1f;
     private float waitTime;
-    private float timer = 0f;
+    private float timer;
 
     void Start()
     {
@@ -23,6 +29,10 @@ public class LevelSectionController : MonoBehaviour
         borderDistance = currCamera.orthographicSize * currCamera.aspect + bufferDistance;
         spawnPosition.x = borderDistance;
         spawnPosition.y = 0f;
+
+        commonSections = Resources.LoadAll("CommonSections", typeof(GameObject)).Cast<GameObject>().ToArray();
+        dropletSections = Resources.LoadAll("DropletSections", typeof(GameObject)).Cast<GameObject>().ToArray();
+        sunSections = Resources.LoadAll("SunSections", typeof(GameObject)).Cast<GameObject>().ToArray();
     }
 
     void FixedUpdate()
@@ -43,7 +53,14 @@ public class LevelSectionController : MonoBehaviour
     {
         if (currCamera.enabled && currCamera.orthographic)
         {
+            section = SelectSection();
             Object.Instantiate(section, spawnPosition, Quaternion.identity);
+            sectionCounter++;
         }
+    }
+
+    GameObject SelectSection()
+    {
+        return commonSections[Random.Range(0, commonSections.Length)];
     }
 }
