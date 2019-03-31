c2453d1c2ab5e92107f047e1e3d7e2165297cb91
Flaviu Bogdan Condor <jaguaru40@yahoo.es>
2019-03-31 03:57:03 +0200

    various sprites added

---
+++ Endless Run/Hito 5/Assets/Scripts/LevelSections/Disapear.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disapear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("LowerPlatform", 3f, 0.5f);
+    }
+
+    // Update is called once per frame
+    private void LowerPlatform()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.25f, transform.position.z); 
+    }
+}
