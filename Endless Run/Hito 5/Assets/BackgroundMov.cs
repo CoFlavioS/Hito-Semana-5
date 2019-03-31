1f81dbe2e6ec580c6676086d6bb9641df7e397af
Flaviu Bogdan Condor <jaguaru40@yahoo.es>
2019-03-31 16:08:06 +0200

    Final test, added background

---
+++ Endless Run/Hito 5/Assets/BackgroundMov.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMov : MonoBehaviour
+{
+    float scrollSpeed = 5f;
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+    
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 36);
+        transform.position = startPos + Vector3.left * newPos;
+    }
+}
