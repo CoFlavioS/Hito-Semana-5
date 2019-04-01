f226d98e24fb4d5252eb00aa7fa01fde437bfc2a
Flaviu Bogdan Condor <jaguaru40@yahoo.es>
2019-04-01 14:08:10 +0200

    Produced game, corrected background

--- Endless Run/Hito 5/Assets/Scripts/BackgroundMov.cs
+++ Endless Run/Hito 5/Assets/Scripts/BackgroundMov.cs
@@ -14,7 +14,7 @@ public class BackgroundMov : MonoBehaviour
     
     void Update()
     {
-        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 36);
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 35.5f);
         transform.position = startPos + Vector3.left * newPos;
     }
 }
